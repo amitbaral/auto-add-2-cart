a9da4334bc20d89f8fc4d9952a460d3584d90ef7
Amit Baral <abaral@networkten.com.au>
2026-01-12 15:38:24 +1100

    Simplify handler to test function

--- api/handler.js
+++ api/handler.js
@@ -1,6 +1,8 @@
-import { createRequestHandler } from "@react-router/node";
-
-// Import the server build
-const build = await import("../build/server/index.js");
-
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default function handler(req, res) {
+    // For now, return a simple response to test if the function works
+    res.status(200).json({
+        status: "ok",
+        message: "Auto Add2Cart API is running",
+        path: req.url
+    });
+}
